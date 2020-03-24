c84749a756a4126c6a0611f500bdd5e22152e589
ehoward1 <emma.howard@ucdconnect.ie>
2020-03-24 19:20:54 +0000

    Update Diagrams_for_waiting_times.R

--- Diagrams_for_waiting_times.R
+++ Diagrams_for_waiting_times.R
@@ -27,10 +27,11 @@ MSC$Semester = MSC$Semester %>% factor(levels=c("1","2"), ordered=TRUE)
 MSC$term_week = MSC$term_week %>% factor(levels=c("1","2","3","4","5","6","7","8",
                                                   "9","10","11","12","rev","exam"), labels=c("1","2","3","4","5","6","7","8","9","10","11","12","rev","exam"), ordered=TRUE) 
 MSC$Day = MSC$Day %>% factor(levels=c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"), ordered=TRUE)
+
 # Helps withdiagram axis
 max = nrow(MSC)/2 %>% round()
 
-# Histogram of waiting times
+# Histogram of waiting times (Figure 1 in paper)
 ggplot(data=MSC, aes(wait_time)) + 
   geom_histogram(breaks=seq(0, 100, by = 5),aes(fill=..count..))+
   labs(title="Histogram of Student Waiting Times") +
@@ -59,13 +60,13 @@ give.n = function(x){
 }
 
 #Plot function
-Waiting_time_plot = function(dataset, factor_of_interest, title){ 
+Waiting_time_plot = function(dataset, factor_of_interest, title, x_lab_name){ 
   name = enquo(factor_of_interest)
   
   ggplot(data = dataset, aes(x=factor(!!name), y=wait_time)) +
     ggtitle(title) +
     geom_boxplot(aes(fill=factor(!!name))) + coord_flip() +
-    theme_bw() + ylab('Wait Time for Students (in min)')+xlab(name)+
+    theme_bw() + ylab('Wait Time for Students (in min)')+xlab(x_lab_name)+
     scale_y_continuous(breaks=seq(0, 100, 20))+ 
     theme(
       plot.title = element_text(lineheight=0, color='black', face="bold",size=14),
@@ -79,12 +80,18 @@ Waiting_time_plot = function(dataset, factor_of_interest, title){
 }
 
 # Example boxplots with various variable and datasets
-Waiting_time_plot(dataset = MSC, factor_of_interest = Semester, title = "Wait Time Given by Semester")
-Waiting_time_plot(dataset = MSC_S1, factor_of_interest = Year, title = "Wait Time Given by Year for Semester 1")
-Waiting_time_plot(dataset = MSC_S2, factor_of_interest = Year, title = "Wait Time Given by Year for Semester 2")
-Waiting_time_plot(dataset = MSC, factor_of_interest = number_tutors, title = "Wait Time Given by Number of Tutors")
-Waiting_time_plot(dataset = MSC, factor_of_interest = term_week, title = "Wait Time Given by Term Week")
-Waiting_time_plot(dataset = MSC, factor_of_interest = term_week, title = "Wait Time Given by Hour")
+Waiting_time_plot(dataset = MSC,    factor_of_interest = Semester, title = "Wait Time Given by Semester",
+                  x_lab_name = "Semester")
+Waiting_time_plot(dataset = MSC_S1, factor_of_interest = Year,     
+                  title = "Wait Time Given by Year for Semester 1", x_lab_name = "Year")
+Waiting_time_plot(dataset = MSC_S2, factor_of_interest = Year, 
+                  title = "Wait Time Given by Year for Semester 2", x_lab_name = "Year")
+Waiting_time_plot(dataset = MSC, factor_of_interest = number_tutors, 
+                  title = "Wait Time Given by Number of Tutors", x_lab_name = "Number\nof\nTutors")
+Waiting_time_plot(dataset = MSC, factor_of_interest = term_week, 
+                  title = "Wait Time Given by Term Week", x_lab_name = "Term Week")
+Waiting_time_plot(dataset = MSC, factor_of_interest = Hour, 
+                  title = "Wait Time Given by Hour", x_lab_name = "Hour")
 
 
 # Investigate different years of MSC data
@@ -104,20 +111,37 @@ MSC_Y18_1 = subset(MSC_Y18, Semester==1)
 MSC_Y19 = subset(MSC, Year=="2019")
 MSC_Y19_2 = subset(MSC_Y19, Semester==2)
 
-Waiting_time_plot(dataset = MSC_Y15_1, factor_of_interest = Day, title = "Wait Time in 2015/16 Sem 1")
-Waiting_time_plot(dataset = MSC_Y16_1, factor_of_interest = Day, title = "Wait Time in 2016/17 Sem 1")
-Waiting_time_plot(dataset = MSC_Y17_1, factor_of_interest = Day, title = "Wait Time in 2017/18 Sem 1")
-Waiting_time_plot(dataset = MSC_Y18_1, factor_of_interest = Day, title = "Wait Time in 2018/19 Sem 1")
+Waiting_time_plot(dataset = MSC_Y15_1, factor_of_interest = Day, title = "Wait Time in 2015/16 Sem 1" ,
+                  x_lab_name = "Day")
+Waiting_time_plot(dataset = MSC_Y16_1, factor_of_interest = Day, title = "Wait Time in 2016/17 Sem 1",
+                  x_lab_name = "Day")
+Waiting_time_plot(dataset = MSC_Y17_1, factor_of_interest = Day, title = "Wait Time in 2017/18 Sem 1",
+                  x_lab_name = "Day")
+Waiting_time_plot(dataset = MSC_Y18_1, factor_of_interest = Day, title = "Wait Time in 2018/19 Sem 1",
+                  x_lab_name = "Day")
 
-Waiting_time_plot(dataset = MSC_Y15_2, factor_of_interest = Day, title = "Wait Time in 2014/15 Sem 2")
-Waiting_time_plot(dataset = MSC_Y16_2, factor_of_interest = Day, title = "Wait Time in 2015/16 Sem 2")
-Waiting_time_plot(dataset = MSC_Y17_2, factor_of_interest = Day, title = "Wait Time in 2016/17 Sem 2")
-Waiting_time_plot(dataset = MSC_Y18_2, factor_of_interest = Day, title = "Wait Time in 2017/18 Sem 2")
-Waiting_time_plot(dataset = MSC_Y19_2, factor_of_interest = Day, title = "Wait Time in 2018/19 Sem 2")
+Waiting_time_plot(dataset = MSC_Y15_2, factor_of_interest = Day, title = "Wait Time in 2014/15 Sem 2", 
+                  x_lab_name = "Day") 
+Waiting_time_plot(dataset = MSC_Y16_2, factor_of_interest = Day, title = "Wait Time in 2015/16 Sem 2", 
+                  x_lab_name = "Day") 
+Waiting_time_plot(dataset = MSC_Y17_2, factor_of_interest = Day, title = "Wait Time in 2016/17 Sem 2",
+                  x_lab_name = "Day")
+Waiting_time_plot(dataset = MSC_Y18_2, factor_of_interest = Day, title = "Wait Time in 2017/18 Sem 2",
+                  x_lab_name = "Day")
+Waiting_time_plot(dataset = MSC_Y19_2, factor_of_interest = Day, title = "Wait Time in 2018/19 Sem 2",
+                  x_lab_name = "Day")
 
 # Remove revision and exam weeks and see a specific term waiting times then
 MSCnorev=subset(MSC_Y18_1, term_week !="rev" || term_week !="exam")
-Waiting_time_plot(dataset = MSCnorev, factor_of_interest = Day, title = "Wait Time in 2018/19 Sem 1 without revision week")
+Waiting_time_plot(dataset = MSCnorev, factor_of_interest = Day, 
+                  title = "Wait Time in 2018/19 Sem 1 without revision week", x_lab_name = "Day")
 
 
-
+#Diagrams for Paper:
+#Figure 2:
+MSC = subset(MSC, number_tutors>0) # In revision and exam weeks there was an extra couple of hours
+# open for the MSC. Since it was outside normal hours, tutors = 0 according to the timetable (n=90)
+Waiting_time_plot(dataset = MSC, factor_of_interest = number_tutors, title = "Wait Time Given by Number of Tutors", x_lab_name = "Number\nof\nTutors")
+#Figure 3
+Waiting_time_plot(dataset = MSC_Y18_1, factor_of_interest = Day, title = "Wait Time in 2018/19 Sem 1",
+                  x_lab_name = "Day")
