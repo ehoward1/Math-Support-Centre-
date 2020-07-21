9e4153247184f24244ca9ec382343f8530a72703
ehoward1 <emma.howard@ucdconnect.ie>
2020-07-21 10:26:39 +0100

    Update Diagrams_for_waiting_times.R

--- Diagrams_for_waiting_times.R
+++ Diagrams_for_waiting_times.R
@@ -4,6 +4,8 @@
 
 rm(list=ls()) #clear space
 
+# Set working directory
+
 install.packages("dplyr")
 install.packages("magrittr")
 install.packages("ggplot2")
@@ -28,16 +30,14 @@ MSC$term_week = MSC$term_week %>% factor(levels=c("1","2","3","4","5","6","7","8
                                                   "9","10","11","12","rev","exam"), labels=c("1","2","3","4","5","6","7","8","9","10","11","12","rev","exam"), ordered=TRUE) 
 MSC$Day = MSC$Day %>% factor(levels=c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"), ordered=TRUE)
 
-# Helps withdiagram axis
-max = nrow(MSC)/2 %>% round()
 
 # Histogram of waiting times (Figure 1 in paper)
 ggplot(data=MSC, aes(wait_time)) + 
-  geom_histogram(breaks=seq(0, 100, by = 5),aes(fill=..count..))+
-  labs(title="Histogram of Student Waiting Times") +
+  geom_histogram(breaks=seq(0, 100, by = 5), fill="blue", colour = "black")+
+  #labs(title="Histogram of Student Waiting Times") +
   scale_x_continuous(breaks=seq(0, 90, 10))+ 
-  theme_bw()+
-  scale_y_continuous(breaks=seq(0, max, 2000))+ 
+  theme_bw()+ 
+  ylim(0,12000)+
   labs(x="Waiting Time (in Min)", y="Number\nof\nVisits") + 
   theme(
     plot.title = element_text(lineheight=0, color='black', face="bold",size=13),
@@ -45,7 +45,7 @@ ggplot(data=MSC, aes(wait_time)) +
     axis.title.y=element_text(angle=0, vjust = 1, color='black',face='bold', size=13),
     axis.text.x=element_text(angle=0, color='black', size=13),
     axis.text.y=element_text(angle=0, color='black',  size=13)
-  )  +   theme(legend.position = "none") 
+  )  +   theme(legend.position = "none")
 
 
 # Wait Time by Year and Semester 
@@ -54,29 +54,26 @@ ggplot(data=MSC, aes(wait_time)) +
 MSC_S1 = subset(MSC, Semester=="1") # Data only for semester 1
 MSC_S2 = subset(MSC, Semester=="2") # Data only for semester 2
 
-#This function allows number of students to be included on the boxplots
-give.n = function(x){
-  return(c(y = median(x)*1.6, label = length(x))) 
-}
-
 #Plot function
 Waiting_time_plot = function(dataset, factor_of_interest, title, x_lab_name){ 
   name = enquo(factor_of_interest)
   
+  dataMedian <- summarise(group_by(dataset, !!name), MD = length(wait_time)) # To calculate 'n='
+  
   ggplot(data = dataset, aes(x=factor(!!name), y=wait_time)) +
-    ggtitle(title) +
     geom_boxplot(aes(fill=factor(!!name))) + coord_flip() +
     theme_bw() + ylab('Wait Time for Students (in min)')+xlab(x_lab_name)+
     scale_y_continuous(breaks=seq(0, 100, 20))+ 
+    geom_text(data = dataMedian, aes(!!name, 1, label = paste('n=', MD)),
+             position = position_dodge(width = 1), size = 4, vjust = -3.7)+ # need to adjust position of n or can delete this to have no 'n='
     theme(
-      plot.title = element_text(lineheight=0, color='black', face="bold",size=14),
+      plot.title = element_blank(),
       axis.title.x=element_text(angle=0, color='black',face='bold', size=13),
       axis.title.y=element_text(angle=0, vjust = 1, color='black',face='bold', size=13),
       axis.text.x=element_text(angle=0, color='black', size=13),
       axis.text.y=element_text(angle=0, color='black',  size=13)
     )  +   theme(legend.position = "none") +
-    scale_fill_viridis(discrete=TRUE, option="D") +
-    stat_summary(fun.data = give.n, geom = "text", fun.y = median) # Put a # infront to remove numbers from diagram
+    scale_fill_viridis(discrete=TRUE, option="D") 
 }
 
 # Example boxplots with various variable and datasets
@@ -110,31 +107,13 @@ MSC_Y18 = subset(MSC, Year=="2018")
 MSC_Y18_1 = subset(MSC_Y18, Semester==1)
 MSC_Y19 = subset(MSC, Year=="2019")
 MSC_Y19_2 = subset(MSC_Y19, Semester==2)
+MSC_Y19 = subset(MSC, Year=="2019")
+MSC_Y19_1 = subset(MSC_Y19, Semester==1)
 
 Waiting_time_plot(dataset = MSC_Y15_1, factor_of_interest = Day, title = "Wait Time in 2015/16 Sem 1" ,
                   x_lab_name = "Day")
 Waiting_time_plot(dataset = MSC_Y16_1, factor_of_interest = Day, title = "Wait Time in 2016/17 Sem 1",
                   x_lab_name = "Day")
-Waiting_time_plot(dataset = MSC_Y17_1, factor_of_interest = Day, title = "Wait Time in 2017/18 Sem 1",
-                  x_lab_name = "Day")
-Waiting_time_plot(dataset = MSC_Y18_1, factor_of_interest = Day, title = "Wait Time in 2018/19 Sem 1",
-                  x_lab_name = "Day")
-
-Waiting_time_plot(dataset = MSC_Y15_2, factor_of_interest = Day, title = "Wait Time in 2014/15 Sem 2", 
-                  x_lab_name = "Day") 
-Waiting_time_plot(dataset = MSC_Y16_2, factor_of_interest = Day, title = "Wait Time in 2015/16 Sem 2", 
-                  x_lab_name = "Day") 
-Waiting_time_plot(dataset = MSC_Y17_2, factor_of_interest = Day, title = "Wait Time in 2016/17 Sem 2",
-                  x_lab_name = "Day")
-Waiting_time_plot(dataset = MSC_Y18_2, factor_of_interest = Day, title = "Wait Time in 2017/18 Sem 2",
-                  x_lab_name = "Day")
-Waiting_time_plot(dataset = MSC_Y19_2, factor_of_interest = Day, title = "Wait Time in 2018/19 Sem 2",
-                  x_lab_name = "Day")
-
-# Remove revision and exam weeks and see a specific term waiting times then
-MSCnorev=subset(MSC_Y18_1, term_week !="rev" || term_week !="exam")
-Waiting_time_plot(dataset = MSCnorev, factor_of_interest = Day, 
-                  title = "Wait Time in 2018/19 Sem 1 without revision week", x_lab_name = "Day")
 
 
 #Diagrams for Paper:
@@ -142,6 +121,16 @@ Waiting_time_plot(dataset = MSCnorev, factor_of_interest = Day,
 MSC = subset(MSC, number_tutors>0) # In revision and exam weeks there was an extra couple of hours
 # open for the MSC. Since it was outside normal hours, tutors = 0 according to the timetable (n=90)
 Waiting_time_plot(dataset = MSC, factor_of_interest = number_tutors, title = "Wait Time Given by Number of Tutors", x_lab_name = "Number\nof\nTutors")
+
 #Figure 3
-Waiting_time_plot(dataset = MSC_Y18_1, factor_of_interest = Day, title = "Wait Time in 2018/19 Sem 1",
-                  x_lab_name = "Day")
+# Remove revision and exam weeks and see a specific term waiting times then
+MSCnorev_18_1=subset(MSC_Y18_1, term_week !="rev")
+Waiting_time_plot(dataset = MSCnorev_18_1, factor_of_interest = Day, 
+                  title = "Wait Time in 2018/19 Sem 1 without revision week", x_lab_name = "Day")
+
+#Figure 4
+# Remove revision and exam weeks and see a specific term waiting times then
+MSCnorev_19_1=subset(MSC_Y19_1, term_week !="rev")
+Waiting_time_plot(dataset = MSCnorev_19_1, factor_of_interest = Day, 
+                  title = "Wait Time in 2019/20 Sem 1 without revision week", x_lab_name = "Day")
+
